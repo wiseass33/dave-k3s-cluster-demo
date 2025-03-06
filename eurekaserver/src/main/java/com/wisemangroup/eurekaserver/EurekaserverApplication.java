000b3c141e84a4ebc75103bae44eaedda336ca47
wiseass33 <david.wiseman@telus.com>
2025-03-05 21:19:59 -0500

    commit

--- eurekaserver/src/main/java/com/wisemangroup/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/wisemangroup/eurekaserver/EurekaserverApplication.java
@@ -2,8 +2,13 @@ package com.wisemangroup.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableEurekaServer
+@Slf4j
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
