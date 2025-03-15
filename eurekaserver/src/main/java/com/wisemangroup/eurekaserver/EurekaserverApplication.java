a7013069b66bd101c2464dfdca16f2f9a3f71679
wiseass33 <david.wiseman@telus.com>
2025-03-15 13:29:59 -0400

    Commit 25

--- eurekaserver/src/main/java/com/wisemangroup/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/wisemangroup/eurekaserver/EurekaserverApplication.java
@@ -6,13 +6,19 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 @EnableEurekaServer
 @Slf4j
 public class EurekaserverApplication {
+	
+	private static final Logger startupLogger = LoggerFactory.getLogger("startup");
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaserverApplication.class, args);
+		startupLogger.info("Welcome:");
 	}
 
 }
