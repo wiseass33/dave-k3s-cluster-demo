f01ad77133388c6e2e607bf6296186bf882838df
wiseass33 <david.wiseman@telus.com>
2025-03-23 11:10:17 -0400

    Config server commit 9

--- configserver/src/main/java/com/wisemangroup/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/wisemangroup/configserver/ConfigserverApplication.java
@@ -1,4 +1,4 @@
-package com.wisemangroup.eurekaserver;
+package com.wisemangroup.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
