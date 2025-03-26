a1cffb5df4133ee82f1220ddc24a204ba34a9f5e
wiseass33 <david.wiseman@telus.com>
2025-03-26 18:57:17 -0400

    commit 22

--- configserver/src/main/java/com/wisemangroup/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/wisemangroup/configserver/ConfigserverApplication.java
@@ -1,13 +1,31 @@
 package com.wisemangroup.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
 
 @SpringBootApplication
+@Slf4j
+@EnableConfigServer
 public class ConfigserverApplication {
 
+	@Value("${spring.application.name}")
+	private String appName;
+	@Value("${spring.profile.active:default}")
+	private String profile;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void logStartupInfo() {
+		log.info("Configserver Application started: name = {}, profile = {}.", appName, profile);
 	}
 
 }
